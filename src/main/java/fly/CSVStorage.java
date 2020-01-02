6d7ceea264bc87dbe165809fb1123719ee9d9504
MishkiGami <mihail.kodolov@gmail.com>
2020-01-02 12:49:15 +0400

    implements Storage in CSVStorage

--- src/main/java/fly/CSVStorage.java
+++ src/main/java/fly/CSVStorage.java
@@ -1,4 +1,35 @@
 package fly;
 
-public class CSVStorage {
+import java.util.List;
+
+public class CSVStorage implements Storage {
+    @Override
+    public void addPlane(Plane plane) throws Exception {
+        
+    }
+
+    @Override
+    public List<Plane> getAllPlanes() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void addAirport(Airports airport) throws Exception {
+
+    }
+
+    @Override
+    public List<Airports> getAllAirports() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void addFly(Fly fly) throws Exception {
+
+    }
+
+    @Override
+    public List<Fly> getAllFly() throws Exception {
+        return null;
+    }
 }
