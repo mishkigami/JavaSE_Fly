403865192d3a82cfd46bd27597083952b79e0228
MishkiGami <mihail.kodolov@gmail.com>
2020-01-02 13:07:57 +0400

    Create method PlanetoCSV

--- src/main/java/fly/CSVStorage.java
+++ src/main/java/fly/CSVStorage.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class CSVStorage implements Storage {
     @Override
     public void addPlane(Plane plane) throws Exception {
-        
+
     }
 
     @Override
@@ -31,5 +31,13 @@ public class CSVStorage implements Storage {
     @Override
     public List<Fly> getAllFly() throws Exception {
         return null;
+    }
+
+    private String planeToCSV (Plane plane) {
+    StringBuilder sb =new StringBuilder();
+    sb.append(plane.getIDPlane()).append(';')
+            .append(plane.getNamePlane()).append(';')
+            .append(plane.getSeats()).append(';');
+    return sb.toString();
     }
 }
